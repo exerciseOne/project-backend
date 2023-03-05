f1591621b293c1a2418d8d66ee9dd8face97f62c
Paweł Ryło <wrx66313@student.wsb.wroclaw.pl>
2023-03-05 19:48:23 +0100

    Add form for display greeting. Install pylint and add job for run it on push.

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('hello.html')
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return render_template('hello.html', name=name)
+
+    return render_template('helloWorldForm.html')
